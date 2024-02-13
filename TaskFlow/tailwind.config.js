7e2414512088096ad252f9a2386e699ee14c9cda
enqer <pawel.kam4@gmail.com>
2024-02-13 21:17:07 +0100

    build: tailwind config

---
+++ TaskFlow/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  mode: 'jit',
+  content: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
