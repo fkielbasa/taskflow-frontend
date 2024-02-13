3902d213b46702e832e2f7d63cbb72c958863db6
Narothe <kuba.dojka@gmail.com>
2024-02-13 23:57:27 +0100

    fix: trying to fix login and register page after pull

--- TaskFlow/src/views/HomeView.vue
+++ TaskFlow/src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+
 </script>
 
 <template>
@@ -6,7 +7,7 @@
     <div class="m-0 w-96 flex flex-col gap-20">
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, perferendis! Delectus perferendis ipsa earum deleniti eligendi velit dolorum natus at corporis ab ut eum mollitia amet eos similique, voluptas, ducimus deserunt, quidem optio iste! Quibusdam rerum ipsum distinctio quae ratione unde voluptatum veritatis officiis accusamus. Labore dicta quam inventore fuga!</p>
-        
+
       <button class="bg-blue px-10 py-2 rounded-sm text-white font-bold">Zacznij już teraz po przez założenie konta</button>
       </div>
     <div>
