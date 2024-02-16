b65e36a836ae9f95e617e9379a7153ec243e1a69
fkielbasa <126203484+fkielbasa@users.noreply.github.com>
2024-02-16 12:54:06 +0100

    style: email, password validation

--- TaskFlow/src/views/LoginView.vue
+++ TaskFlow/src/views/LoginView.vue
@@ -8,7 +8,9 @@ export default {
       showForgotPasswordForm: false,
       message: 'Sign in to your account',
       email: '',
-      password: ''
+      password: '',
+      emailCheck: false,
+      passwordCheck: false
     }
   },
   watch: {
@@ -30,7 +32,7 @@ export default {
         console.log(error);
         return null;
       }
-      
+
     },
     async handleSignOut() {
       try {
@@ -44,7 +46,7 @@ export default {
       axios.post(
         'https://localhost:7084/api/v1/Auth/login',
         {
-          email: this.email, 
+          email: this.email,
           password: this.password
         },
       )
@@ -54,6 +56,8 @@ export default {
       })
       .catch(error => {
         console.error('Error:', error)
+        this.emailCheck = true;
+        this.passwordCheck = true;
       });
     }
   },
@@ -74,16 +78,16 @@ export default {
       <img class="mx-auto h-10 w-auto" src="@/assets/logo/TaskFlowFullLogo.png" alt="main logo">
       <h2 class="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">{{ message }}</h2>
     </div>
-
     <div class="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
       <form v-if="!showForgotPasswordForm" class="space-y-6" @submit.prevent="login" method="POST">
         <div>
           <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
           <div class="mt-1">
-            <input id="email" name="email" type="email" autocomplete="email" v-model="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+            <input id="email" name="email" type="email" autocomplete="email" v-model="email" required
+                   :class="[emailCheck ? 'border-input-error' : 'border-0 ',
+                  'block w-full rounded-md py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6']">
           </div>
         </div>
-
         <div>
           <div class="flex items-center justify-between">
             <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
@@ -92,11 +96,14 @@ export default {
             </div>
           </div>
           <div class="mt-1">
-            <input id="password" name="password" type="password" autocomplete="current-password" v-model="password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" >
+            <input id="password" name="password" type="password" autocomplete="current-password" v-model="password" required
+                   :class="[passwordCheck ? 'border-input-error' : 'border-0',
+                   'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6']" >
           </div>
+          <p v-if="emailCheck || passwordCheck" class="text-red-500">Incorrect login or email</p>
         </div>
         <div>
-          <button type="submit" @click="login()" class=" buttonCustomStyle flex w-full justify-center rounded-md px-3 py-1.5  text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+          <button type="submit" class=" buttonCustomStyle flex w-full justify-center rounded-md px-3 py-1.5  text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
         </div>
         <div>
           <button @click="handleSignIn" class="buttonCustomStyle flex  w-full items-center justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
@@ -126,4 +133,8 @@ export default {
 
 <style>
   @import '@/assets/styles/CustomStyles.module.css';
+  .border-input-error {
+    border-width: 2px;
+    border-color: #EF4444;
+  }
 </style>
