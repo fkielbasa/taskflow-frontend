76e1360cd9001f4bb7f0a4f0de55a37efd6d4fcc
fkielbasa <126203484+fkielbasa@users.noreply.github.com>
2024-02-19 15:33:08 +0100

    feat: danger alerts

---
+++ TaskFlow/src/components/DangerAlert.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export default {
+    name: 'Alert',
+    props: ['error'],
+  }
+</script>
+
+<template>
+  <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-1 rounded relative" role="alert">
+    <span class="block sm:inline">{{ error }}</span>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- TaskFlow/src/views/LoginView.vue
+++ TaskFlow/src/views/LoginView.vue
@@ -1,9 +1,12 @@
 <script lang="ts">
  import { inject } from 'vue';
  import axios from 'axios';
+ import Alert from "@/components/DangerAlert.vue";
  import { useRouter } from 'vue-router';
  import {TOKEN_KEY} from "@/config/auth";
 export default {
+  name: 'Login',
+  components: {Alert},
   data() {
     return {
       email: '',
@@ -80,12 +83,12 @@ export default {
     </div>
     <div class="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
       <form class="space-y-6" @submit.prevent="login" method="POST">
+        <Alert v-if="emailCheck" error="Incorrect login or email"></Alert>
         <div>
           <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
           <div class="mt-1">
             <input id="email" name="email" type="email" autocomplete="email" v-model="email" required
-                   :class="[emailCheck ? 'border-input-error' : 'border-0 ',
-                  'block w-full rounded-md py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6']">
+                   class='block w-full rounded-md py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'>
           </div>
         </div>
         <div>
@@ -97,10 +100,8 @@ export default {
           </div>
           <div class="mt-1">
             <input id="password" name="password" type="password" autocomplete="current-password" v-model="password" required
-                   :class="[passwordCheck ? 'border-input-error' : 'border-0',
-                   'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6']" >
+                   class='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'>
           </div>
-          <p v-if="emailCheck || passwordCheck" class="text-red-500">Incorrect login or email</p>
         </div>
         <div>
           <button type="submit" class=" buttonCustomStyle flex w-full justify-center rounded-md px-3 py-1.5  text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
