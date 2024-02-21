9081f5f41d76e4e5dcb90d97df64fc86b15bcbf5
enqer <pawel.kam4@gmail.com>
2024-02-22 00:06:18 +0100

    fix: active section

--- TaskFlow/src/components/Navbar.vue
+++ TaskFlow/src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav 
-    class="m-0  flex justify-between w-screen h-16 fixed top-0 left-0 z-10"
+    class="nav m-0  flex justify-between w-screen h-16 fixed top-0 left-0 z-10"
     :class="{'bg-black': setColorChange, 'bg-transparent': !setColorChange}"
   >
     <ul class="flex">
@@ -11,7 +11,7 @@
         class="py-0 cursor-pointer m-auto h-full  px-6 flex items-center border-b-2 border-transparent text-grey hover:text-blue transition-all duration-500"
         @click="scrollToSection(section)"
       >
-          <NavItem :section="section" ></NavItem>
+          <NavItem :section="section" />
       </li>
     </ul>
     <div class=" mx-4">
@@ -28,30 +28,56 @@
     </div>
   </nav>
 </template>
-
 <script setup lang="ts">
 import { ref } from 'vue';
 import NavItem from './NavItem.vue'
 import Logo from './Logo.vue';
 
-
 const sections: string[] = ['Home', 'About', 'Authors']
 const setColorChange = ref(false)
 
 const setActiveSection = () => {
-  sections.forEach((section, index) => {
+  const windowHeight = window.innerHeight;
+  let currentActiveIndex = -1;
+
+  for (let index = 0; index < sections.length; index++) {
+    const section = sections[index];
     const element = document.getElementById(section);
-    const listItem = document.querySelectorAll('nav li')[index];
-    const frontPage = document.getElementById('frontPage')
 
-    if (element && (element.offsetTop <= window.scrollY + 60) && 
-        (element.offsetTop + element.offsetHeight > window.scrollY + 60)) {
-      listItem.classList.add('!text-blue');
-    } else {
-      listItem.classList.remove('!text-blue');
+    if (element) {
+      const topOffset = element.offsetTop;
+      const bottomOffset = topOffset + element.offsetHeight;
+      const halfSectionHeight = element.offsetHeight / 2;
+      const sectionAboveViewport = bottomOffset - halfSectionHeight <= window.scrollY + 60;
+      const sectionBelowViewport = topOffset + halfSectionHeight >= window.scrollY + windowHeight;
+      const sectionHalfInView = !sectionAboveViewport && !sectionBelowViewport;
+      if (sectionHalfInView) {
+        currentActiveIndex = index;
+        break;
+      }
     }
+  }
+  if (currentActiveIndex === -1) {
+    for (let index = 0; index < sections.length; index++) {
+      const section = sections[index];
+      const element = document.getElementById(section);
 
-  })
+      if (element) {
+        const topOffset = element.offsetTop;
+        if (topOffset >= window.scrollY + 60) {
+          currentActiveIndex = index;
+          break;
+        }
+      }
+    }
+  }
+  document.querySelectorAll('.nav li').forEach(item => {
+    item.classList.remove('!text-blue');
+  });
+  if (currentActiveIndex !== -1) {
+    const activeListItem = document.querySelectorAll('.nav li')[currentActiveIndex];
+    activeListItem.classList.add('!text-blue');
+  }
 }
 window.addEventListener('scroll', setActiveSection);
 
@@ -75,6 +101,5 @@ const scrollToSection = (id: string)  => {
     });
   }
 }
-
 
 </script>
