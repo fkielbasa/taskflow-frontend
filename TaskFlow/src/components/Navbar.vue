f055a41ef3d129c3f6c96f0632a82ccea607b841
enqer <pawel.kam4@gmail.com>
2024-02-21 17:19:32 +0100

    feat: set bg-color  nav  on scroll

--- TaskFlow/src/components/Navbar.vue
+++ TaskFlow/src/components/Navbar.vue
@@ -1,6 +1,8 @@
 <template>
-  <nav class="m-0 bg-white flex justify-between w-screen h-16
-              shadow-md fixed top-0 left-0">
+  <nav class="m-0 bg-transparent flex justify-between w-screen h-16
+               fixed top-0 left-0"
+      :class="{'bg-white': setColorChange}"
+               >
     <ul class="flex">
       <img  alt="" class="logo mx-10" src="@/assets/logo/TaskFlowSmallLogo.png" width="50" height="50">
       <li 
@@ -11,11 +13,10 @@
       >
           <NavItem :section="section" ></NavItem>
       </li>
-      <p><a href="https://storyset.com/web">Web illustrations by Storyset</a></p>
     </ul>
-    <div>
+    <div class=" mx-4">
       <RouterLink to="/register">
-        <button class="text-grey mx-2 px-4 py-2 text-lg h-full font-medium">Register</button>
+        <button class="text-blue mx-2 px-4 py-2 text-lg h-full font-medium">Register</button>
       </RouterLink>
       <RouterLink to="/login" >
         <!-- <button  class="ml-2 mr-0 px-10 py-2 bg-blue text-white h-full font-medium text-lg">Login</button> -->
@@ -26,14 +27,17 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import NavItem from './NavItem.vue'
 
 const sections: string[] = ['Home', 'About', 'Authors']
+const setColorChange = ref(false)
 
 const setActiveSection = () => {
   sections.forEach((section, index) => {
     const element = document.getElementById(section);
     const listItem = document.querySelectorAll('nav li')[index];
+    const frontPage = document.getElementById('frontPage')
 
     if (element && (element.offsetTop <= window.scrollY + 20) && 
         (element.offsetTop + element.offsetHeight > window.scrollY + 20)) {
@@ -48,6 +52,14 @@ const setActiveSection = () => {
 }
 window.addEventListener('scroll', setActiveSection);
 
+const changeNavbarColor = () => {
+  if (window.scrollY >= 70) { 
+    setColorChange.value = true;  
+  } else {
+    setColorChange.value = false;
+  } 
+};
+window.addEventListener("scroll", changeNavbarColor);
 
 
 const scrollToSection = (id: string)  => {
