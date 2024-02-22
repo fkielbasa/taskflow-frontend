889a8cbd8c4983da06165886d73753075e7661f5
enqer <pawel.kam4@gmail.com>
2024-02-22 16:00:35 +0100

    style: responsive footer

--- TaskFlow/src/components/Footer.vue
+++ TaskFlow/src/components/Footer.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="bg-full_black flex justify-center relative py-4">
+    <div class="bg-full_black flex flex-col items-center justify-center relative py-4">
         <div class="flex flex-col items-center">
-            <logo />
-            <p class=" text-lg font-semibold text-grey">© TaskFlow. Wszelkie prawa zastrzeżone.</p>
+            <logo class="size-16 min-[1920px]:size-[80px]" />
+            <p class=" text-center text-lg font-semibold text-grey xl:text-2xl min-[1920px]:text-3xl">© TaskFlow. Wszelkie prawa zastrzeżone.</p>
         </div>
-        <p class=" text-grey absolute right-0 bottom-1/3 pr-2"><a href="https://storyset.com/web">Web illustrations by Storyset</a></p> 
+        <p class="font-semibold text-grey pr-2 xl:text-xl min-[1920px]:text-2xl"><a href="https://storyset.com/web">Web illustrations by Storyset</a></p> 
     </div>
 </template>
 <script setup lang="ts">
